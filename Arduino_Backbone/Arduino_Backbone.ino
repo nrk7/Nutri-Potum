b5cc8f7e04e5289fdf0ef5873420e967ebdb2d53
nrk7 <nitish.katta@gmail.com>
2015-12-17 11:16:27 -0600

    Pseudocode for the Arduino portion of the Nutri-Potum machine

---
+++ Arduino_Backbone/Arduino_Backbone.ino
@@ -0,0 +1,27 @@
+void setup() {
+  // put your setup code here, to run once: 
+  // Set up constants, i.e. amount of protein, carbohydrates, calories, etc. --> This information will come from Raspberry Pi
+  // Assign motor/valve for protein powder
+  // Assign motor/valve for carbohydrate powder
+  // Assign motor/valve for flavor powder
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // While (original scale reading for protein) - (new scale reading) < amount of protein desired
+      // turn proper motor to release more protein
+  // Update number of calories
+
+  // While (original scale reading for carbohydrates) - (new scale reading) < amount of carbohydrates desired
+      // turn proper motor to release more carbohydrates
+  // Update number of calories 
+
+  // While (original scale reading for flavor powder) - (new scale reading) < amount of flavor desired
+      // turn proper motor to release more flavor
+  // Update number of calories   
+
+  // Repeat for desired number of powders
+  
+
+}
